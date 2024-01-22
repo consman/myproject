a8b7dc623bbf389bebfe2a31f5e93e240c53a83f
bob <bob.dev.oak@gmail.com>
2024-01-22 09:50:33 -0800

    ran ng generate environments and set up production with no luck.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {environment} from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'myproject';
+	constructor(){
+		console.log('environment.production: ' + environment.production); 
+	}
 }
